ccd50cbee67d37c79386220594da18bfebb57ce2
TAEGON <brayle0106@gmail.com>
2022-05-13 20:29:22 +0900

    sort option 설정 / string 비교

---
+++ 가장 큰 수.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(const string& a, const string& b) {
+    return (a + b) > (b + a);
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> tmp;
+
+    for (int i : numbers) tmp.push_back(to_string(i));
+
+    sort(tmp.begin(), tmp.end(), cmp);
+
+    for (string i : tmp) answer += i;
+
+    if (answer[0] == '0') answer = "0";
+    return answer;
+}
