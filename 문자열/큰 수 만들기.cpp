2fa107a0e83094725f4effad118e47c7224902bf
TAEGON <brayle0106@gmail.com>
2022-05-19 08:25:30 +0900

    substr / 길이가 같은 문자열 비교

---
+++ 문자열/큰 수 만들기.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+bool cmp(const char a, const char b) {
+	return a >= b;
+}
+
+string solution(string number, int k) {
+	int pos;
+	while (k > 0)
+	{
+		pos = 0;
+		for (int i = 0; i <= number.size() - 2; ++i)
+		{
+			if (!cmp(number[i], number[i + 1])) break;
+			else ++pos;
+		}
+		if (pos == number.size() - 1) number = number.substr(0, pos);
+		else number = number.substr(0, pos) + number.substr(pos + 1);
+		--k;
+	}
+
+	return number;
+}
