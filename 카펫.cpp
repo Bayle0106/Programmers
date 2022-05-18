18f69922bef08488ce8fa3e7f86a76ce86115b05
TAEGON <brayle0106@gmail.com>
2022-05-18 23:17:36 +0900

    sort / compare

---
+++ 카펫.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+	vector<int> answer;
+
+	for (int i = 1; i <= sqrt(yellow); ++i)
+	{
+		if (yellow % i != 0) continue;
+
+		int w, h;
+		w = yellow / i;
+		h = i;
+
+		if (2 * (w + h + 2) == brown)
+		{
+			answer.push_back(w + 2);
+			answer.push_back(h + 2);
+			break;
+		}
+	}
+	sort(answer.begin(), answer.end(), greater<int>());
+
+	return answer;
+}
