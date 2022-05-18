36acacbe4fa7512e5bcfb1b04ba2669db59372fe
TAEGON <brayle0106@gmail.com>
2022-05-18 22:58:19 +0900

    vector / sort

---
+++ H-index(algorithm).cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int solution(vector<int> citations) {
+	int answer = 0;
+
+	// 오름차순 배열
+	sort(citations.begin(), citations.end());
+
+	answer = citations.size();
+	for (int tmp : citations)
+	{
+		if (tmp < answer) --answer;
+		else break;
+	}
+
+	return answer;
+}
