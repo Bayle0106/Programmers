5469ae31a7aec2b29af565e70df8fa1a3fff500e
TAEGON <brayle0106@gmail.com>
2022-05-20 14:50:42 +0900

    map을 이용한 문자열(key) 별 개수관리

---
+++ map & set & pq/보석 쇼핑.cpp
@@ -0,0 +1,32 @@
+﻿#include<vector>
+#include<string>
+#include<map>
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+	vector<int> answer(2, 0);
+	int head = 0;
+	int tail = 0;
+	int len = 987654321;
+	map<string, int> m;
+	for (string str : gems)
+	{
+		// 새로운 값: answer 초기화
+		if (m.find(str) == m.end()) len = 987654321;
+
+		++m[str];
+		++tail;
+
+		// head가 가리키는 원소의 개수가 여러개
+		while (m[gems[head]] > 1) --m[gems[head++]];
+
+		if (tail - head < len)
+		{
+			len = tail - head;
+			answer[0] = head + 1;
+			answer[1] = tail;
+		}
+	}
+	return answer;
+}
